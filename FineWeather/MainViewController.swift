daad0742b9e0aa3710f791c144f0c5d440c8f0ca
gggno <geunhojeong.jgh@gmail.com>
2022-12-19 17:13:31 +0900

    스크롤뷰 작동

--- FineWeather/MainViewController.swift
+++ FineWeather/MainViewController.swift
@@ -16,32 +16,78 @@ class MainViewController: UIViewController {
         self.view.backgroundColor = .systemYellow
         self.title = "경기도 부천시"
         
+        let titleView: UIView = {
+            let view = UIView()
             
+            view.backgroundColor = .gray
+            
+            return view
+        }()
         
-        let contentLabel: UILabel = {
-            let label = UILabel()
-            label.numberOfLines = 0
-            label.text = "없으면, 않는 고행을 피는 철환하였는가? 것은 꽃 있는 풍부하게 목숨을 구하기 거선의 봄바람이다. 인생에 가는 관현악이며, 열매를 피고 동력은 모래뿐일 것이다. 타오르고 위하여 불러 살았으며, 그들에게 가는 노래하며 투명하되 인간의 아니다. 천고에 위하여, 일월과 힘차게 이상 위하여서. 방황하여도, 가슴에 풍부하게 황금시대의 만천하의 그들에게 부패뿐이다. 용기가 뼈 따뜻한 열락의 영원히 꽃이 들어 아름답고 기쁘며, 피다. 피고, 내려온 만물은 예수는 밥을 불러 봄바람이다. 눈에 노래하며 행복스럽고 이것이다. 것이 살 것이다.보라, 따뜻한 힘있다끓는 남는 석가는 이성은 바이며, 든 그리하였는가? 청춘의 살았으며, 거선의 가는 이 무엇을 이것이다. 황금시대의 있는 오직 군영과 이상의 온갖 피어나기 원대하고, 주는 것이다. 위하여, 생생하며, 놀이 찬미를 스며들어 있음으로써 구하지 때문이다. 불러 어디 어디 우는 방황하여도, 것은 자신과 가치를 튼튼하며, 교향악이다. 얼음이 고동을 더운지라 얼음과 천지는 밥을 예가 부패뿐이다. 것은 창공에 튼튼하며, 것은 놀이 이것이야말로 우리는 하였으며, 있으랴? 하여도 어디 황금시대를 별과 피에 옷을 품었기 천하를 끓는 사막이다. 불어 대고, 보내는 싸인 산야에 황금시대다.어디 듣기만 이상 웅대한 뭇 교향악이다. 무엇을 못할 피는 사라지지 보이는 커다란 것이다. 수 이성은 못하다 생명을 옷을 과실이 사막이다. 유소년에게서 그러므로 행복스럽고 피는 얼마나 인간의 철환하였는가? 인간의 얼음 이상 무엇이 않는 힘있다. 보배를 영락과 얼음이 인간에 무엇이 바이며, 무한한 보라. 타오르고 위하여, 인간에 것이다. 만천하의 사랑의 얼마나 가는 그들에게 눈에 인생에 사막이다. 청춘은 가슴이 청춘의 열매를 무엇이 열락의 풀이 뿐이다. 위하여, 별과 심장은 이것은 석가는 이상은 피고, 힘있다. 곳이 청춘의 미인을 그들의 장식하는 아름다우냐?충분히 모래뿐일 이상, 하는 우는 웅대한 그들에게 가치를 기관과 끓는다. 그들은 곧 싸인 튼튼하며, 풍부하게 새 듣는다. 너의 힘차게 사랑의 대중을 철환하였는가? 목숨이 온갖 두손을 가진 사막이다. 가슴에 얼마나 청춘의 이상 때까지 무엇을 있으랴? 우는 사라지지 기관과 있는가? 발휘하기 이상 피가 군영과 있음으로써 돋고, 희망의 수 황금시대를 약동하다. 그들에게 열락의 같이, 하였으며, 위하여, 주며, 방황하였으며, 위하여, 때문이다. 끝에 크고 아름답고 사막이다. 있는 그림자는 지혜는 청춘의 뿐이다. 위하여 소담스러운 힘차게 맺어, 얼음에 청춘의 인생에 노래하며 위하여서. 위하여, 대고, 남는 튼튼하며, 말이다. 새 그들의 웅대한 아니다. 이상의 보는 불러 따뜻한 이상의 맺어, 어디 얼마나 얼마나 철환하였는가? 살 별과 못하다 인간의 무한한 듣는다. 튼튼하며, 이상은 역사를 보라. 어디 밥을 그러므로 청춘의 피부가 크고 가치를 청춘의 창공에 아름다우냐? 위하여, 굳세게 것은 붙잡아 어디 인생에 같은 이것이다. 피고 위하여서, 돋고, 방지하는 사막이다.바로 있는 지혜는 인도하겠다는 그들은 그러므로 오직 풍부하게 봄바람이다. 것이 그들에게 우리의 품으며, 수 희망의 칼이다. 천지는 그와 놀이 이상의 소금이라 품에 별과 말이다. 이상 피고 인간은 황금시대를 청춘을 실현에 공자는 더운지라 그리하였는가? 그것은 인생에 눈이 우리의 옷을 가지에 청춘이 귀는 그들의 칼이다. 풍부하게 인생의 충분히 발휘하기 아니다. 소담스러운 더운지라 간에 그들은 이상이 있는 그들의 운다. 가지에 이것을 군영과 희망의 고행을 보이는 있음으로써 사람은 끓는다. 같이, 같은 대한 철환하였는가? 새 우는 공자는 우리의 유소년에게서 이것이다."
-            label.translatesAutoresizingMaskIntoConstraints = false
+        let titleView2: UIView = {
+            let view = UIView()
             
-            return label
+            view.backgroundColor = .gray
+            
+            return view
+        }()
+        
+        let titleView3: UIView = {
+            let view = UIView()
+            
+            view.backgroundColor = .gray
+            
+            return view
+        }()
+        
+        let titleView4: UIView = {
+            let view = UIView()
+            
+            view.backgroundColor = .gray
+            
+            return view
         }()
         
         let containerView: UIView = {
             let view = UIView()
             
             view.backgroundColor = .red
-            view.addSubview(contentLabel)
-            
+            view.addSubview(titleView)
+            view.addSubview(titleView2)
+            view.addSubview(titleView3)
+            view.addSubview(titleView4)
+
             return view
         }()
         
-        NSLayoutConstraint.activate([
-            contentLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            contentLabel.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
-            contentLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 0),
-            contentLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0)
-        ])
+        titleView.snp.makeConstraints { make in
+            make.size.equalTo(180)
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(containerView.snp.leading).offset(30)
+        }
+        
+        titleView2.snp.makeConstraints { make in
+            make.size.equalTo(240)
+            make.top.equalTo(titleView.snp.bottom).offset(40)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(containerView.snp.leading).offset(30)
+        }
+        
+        titleView3.snp.makeConstraints { make in
+            make.size.equalTo(340)
+            make.top.equalTo(titleView2.snp.bottom).offset(40)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(containerView.snp.leading).offset(30)
+        }
+        
+        titleView4.snp.makeConstraints { make in
+            make.size.equalTo(440)
+            make.top.equalTo(titleView3.snp.bottom).offset(40)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(containerView.snp.leading).offset(30)
+            make.bottom.equalTo(containerView.snp.bottom).offset(-30)
+        }
         
         let scrollView: UIScrollView = {
             let scroll = UIScrollView()
