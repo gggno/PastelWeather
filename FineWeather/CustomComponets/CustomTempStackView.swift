dad8a0c36aad3e4b03a35d3637f1192a206163d3
gggno <geunhojeong.jgh@gmail.com>
2023-01-06 23:06:31 +0900

    옵셔널 처리

--- FineWeather/CustomComponets/CustomTempStackView.swift
+++ FineWeather/CustomComponets/CustomTempStackView.swift
@@ -38,7 +38,7 @@ class CustomTempStackView: UIStackView {
                     self.tempLabel.text = maxTmp + "˚"
                 } else {
                     self.tempNameLabel.text = tempName + ":"
-                    self.tempLabel.text = "_ _" + "˚"
+                    self.tempLabel.text = "_ _"
                 }
             }
             
@@ -50,7 +50,7 @@ class CustomTempStackView: UIStackView {
                     self.tempLabel.text = minTmp + "˚"
                 } else {
                     self.tempNameLabel.text = tempName + ":"
-                    self.tempLabel.text = "_ _" + "˚"
+                    self.tempLabel.text = "_ _"
                 }
             }
             
@@ -74,26 +74,21 @@ class CustomTempStackView: UIStackView {
                 
                 guard let tmp = response.response?.body?.items.item[index].fcstValue else { return }
                 guard let wsd = response.response?.body?.items.item[index+4].fcstValue else { return }
-                print(tmp, wsd)
                 
                 let t = Double(tmp)! // 온도
                 let v = pow(Double(wsd)!, 0.16) // 풍속
                 
-                var tmpCal: Double?
-                tmpCal = round(13.12 + 0.6215 * t - 11.37 * v + 0.3965 * v * t)
+                let tmpCal: Double = round(13.12 + 0.6215 * t - 11.37 * v + 0.3965 * v * t)
+                let feelTmpSTr: String? = String(Int(tmpCal))
                 
-                print(String(Int(tmpCal!)))
-                self.tempNameLabel.text = tempName + ":"
-                self.tempLabel.text = String(Int(tmpCal!)) + "˚"
-                
-//                if let feelTmp = tmpCal {
-//                    // 현재온도
-//                    self.tempNameLabel.text = tempName + ":"
-//                    self.tempLabel.text = feelTmp + "˚"
-//                } else {
-//                    self.tempNameLabel.text = tempName + ":"
-//                    self.tempLabel.text = "_ _" + "˚"
-//                }
+                if let feelTmp = feelTmpSTr {
+                    self.tempNameLabel.text = tempName + ":"
+                    self.tempLabel.text = feelTmp + "˚"
+                    
+                } else {
+                    self.tempNameLabel.text = tempName + ":"
+                    self.tempLabel.text = "_ _"
+                }
             }
         }
         
